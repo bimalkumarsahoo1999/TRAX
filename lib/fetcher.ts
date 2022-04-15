268d536a260eb6ec4ae587e2a682dfa5c6a86298
bimalkumarsahoo1999 <kumarbimal1999@gmail.com>
2022-04-16 04:05:41 +0530

    backend done

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,3 +1,5 @@
+import { RangeSlider } from '@chakra-ui/react'
+
 export default function fetcher(url: String, data = undefined) {
   return fetch(`${window.location.origin}/api${url}`, {
     method: data ? 'POST' : 'GET',
@@ -6,5 +8,10 @@ export default function fetcher(url: String, data = undefined) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
+  }).then((res) => {
+    if (res.status > 399 && res.status < 200) {
+      throw new Error()
+    }
+    return res.json()
   })
 }
